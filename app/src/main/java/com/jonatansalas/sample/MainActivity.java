2d92ce84c1fef99dadb66f5ba79656f18fca25f4
jonatan <jonatansalas@live.com.ar>
2016-03-19 15:13:28 -0300

    Updated DayView class

--- app/src/main/java/com/jonatansalas/sample/MainActivity.java
+++ app/src/main/java/com/jonatansalas/sample/MainActivity.java
@@ -121,9 +121,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
                 if (dayTime.getDay() == 7 && dayTime.getMonth() == 2 && dayTime.getYear() == 2016 && dayTime.isCurrentMonth()) {
                     dayView.setTextColor(Color.BLACK);
+                    dayView.setBackgroundColor(Color.CYAN);
                 }
-
-                dayView.drawRipples();
             }
         });
     }
--- library/src/main/java/com/jonatansalas/materialcalendarview/widget/DayView.java
+++ library/src/main/java/com/jonatansalas/materialcalendarview/widget/DayView.java
@@ -1,6 +1,8 @@
 package com.jonatansalas.materialcalendarview.widget;
 
 import android.content.Context;
+import android.support.annotation.ColorRes;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -17,6 +19,7 @@ public class DayView extends TextView {
 
     public DayView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
+        RippleCompat.init(context.getApplicationContext());
     }
 
     public DayView(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -24,7 +27,15 @@ public class DayView extends TextView {
     }
 
     public void drawRipples() {
-        RippleCompat.init(getContext().getApplicationContext());
-        RippleCompat.apply(this, this.getCurrentTextColor());
+        RippleCompat.apply(this, getDrawingCacheBackgroundColor());
+    }
+
+    public void drawRipples(Integer color) {
+        RippleCompat.apply(this, color);
+    }
+
+    public void drawRipples(@ColorRes int colorId) {
+        final int color = ContextCompat.getColor(getContext(), colorId);
+        RippleCompat.apply(this, color);
     }
 }
